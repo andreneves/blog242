5f747853137275a2e7bf98c9a7e73f1989984903
andreneves <andr@andr.com.br>
2024-09-10 21:36:10 -0300

    auditoria

--- app/Models/Postagem.php
+++ app/Models/Postagem.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Postagem extends Model
+class Postagem extends Model implements Auditable
 {
     use HasFactory;
+
+    use \OwenIt\Auditing\Auditable;
 
     protected $table = 'postagens';
 
